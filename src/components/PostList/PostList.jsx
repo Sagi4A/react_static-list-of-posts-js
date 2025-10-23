ecf650ab0f63391a2717c48439a57487d01799ff
Sagi4A <voitko.katya.1@gmail.com>
2025-10-23 18:59:15 +0300

    tested

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,10 +1,10 @@
 import { PostInfo } from '../PostInfo/PostInfo';
 import './PostList.scss';
 
-export const PostList = ({ posts }) => {
+export const PostList = ({ posts }) => (
   <div className="PostList">
     {posts.map(post => (
       <PostInfo key={post.id} post={post} />
     ))}
-  </div>;
-};
+  </div>
+);
